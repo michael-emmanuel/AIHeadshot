5070a76a1edc5dad5614525b68869e4e316deaee
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-04-21 18:32:16 -0400

    configuring dashboard

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,25 @@
+import { AppSidebar } from '@/components/app-sidebar';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
+
+// anything placed in this file will render for all the routes in dashboard group
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <div>
+          <SidebarTrigger className='-ml-1' />
+        </div>
+        <main className='flex flex-1 flex-col gap-4 p-4 pt-0'>{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
